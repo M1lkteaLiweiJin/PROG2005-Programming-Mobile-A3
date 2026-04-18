1462fa5b0feb860d9dd60750ebd79124ac48044c
M1lkteaLiweiJin <1778257879@qq.com>
2026-04-18 14:18:23 +0800

    Add files via upload

---
+++ inventory-app/src/main.ts
@@ -0,0 +1,21 @@
+/**
+ * main.ts - Application entry point
+ * Bootstraps the standalone AppComponent with Ionic, HTTP client and routing.
+ * Author: Kangzhe Zhao
+ */
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({ mode: 'md' }),
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+}).catch((err: unknown) => console.error(err));
